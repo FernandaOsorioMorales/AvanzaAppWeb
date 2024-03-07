cb53381c1dcba881f2946e7969d96a16e5f14362
angelAlk <angelalk@ciencias.unam.mx>
2024-03-07 15:34:09 -0600

    Ops: Setup Docker and Docker Compose
    Created a single Dockerfile that compiles both the
    backend and frontend for the project. After compilation
    the frontend is configured to be served statically from the backend.
    
    Changed frontend bundler to Vite, to ease serving from backend.
    
    Configured docker compose with our web app and a postgress database.
    For now the database uses a .env file for the password.

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-//	"fmt"
-//	"log"
-//	"net/http"
-//	"os"
+	"os"
+	"log"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -13,9 +12,19 @@ func main() {
 	app := fiber.New()
 
 	app.Static("/static", "/app/static")
+	//kk
+	entries, e := os.ReadDir("/app/static")
+	if e != nil {
+		log.Fatal(e)
+	}
+	//t
+	for _,e := range entries {
+		fmt.Println(e.Name())
+	}
+
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world")
+		return c.SendString("Good night")
 	})
 
 	app.Listen(":9090")
