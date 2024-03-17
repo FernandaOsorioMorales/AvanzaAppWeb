12d7fad954cc719acd51dde1bb0ec7c6426ac232
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2024-03-17 13:17:46 -0600

    Add chat routes: getContacts and message handler (not complete yet)

---
+++ backend/routes/chat.routes.go
@@ -0,0 +1,57 @@
+package routes
+
+import (
+	"backend/controllers"
+	"backend/tools"
+	"backend/tools/db"
+
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+)
+
+// TODO - Get email from session
+
+// Returns the user contacts based on the user session.
+func GetContacts(c *fiber.Ctx) error {
+	dbase := db.Orm()
+	_, sessErr := tools.GetCurrentSession(c)
+
+	if sessErr != nil {
+		return controllers.ApiError(c, "Failed to get session", 500)
+	}
+
+	mail := "¡¡¡REPLACE WITH EMAIL FROM SESSION!!!" // TODO - Get email from session
+	contactList, err := controllers.GetContactList(dbase, mail)
+
+	if err != nil {
+		return controllers.ApiError(c, "Failed to retrieve user contacts", 500)
+	}
+
+	return c.JSON(fiber.Map{
+		"contacts": contactList,
+	})
+}
+
+//TODO - Handle errors
+//TODO - Logging
+
+// Send message to the specified contact using websockets.
+func MessageHandler(c *websocket.Conn) {
+	var (
+		mt      int
+		message []byte
+		err     error
+	)
+	for {
+		mt, message, err = c.ReadMessage()
+
+		if err != nil {
+			break
+		}
+
+		err = c.WriteMessage(mt, message)
+		err = c.WriteMessage(mt, []byte("Te has unido al chat"))
+
+	}
+
+}
