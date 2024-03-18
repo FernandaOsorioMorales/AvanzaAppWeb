7080782c241d024b4f9d4c5b5620495cd8eb569b
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2024-03-18 12:25:48 -0600

    Updatte chat routes: message habndler is now working

--- backend/routes/chat.routes.go
+++ backend/routes/chat.routes.go
@@ -2,8 +2,13 @@ package routes
 
 import (
 	"backend/controllers"
+	"backend/models"
 	"backend/tools"
 	"backend/tools/db"
+	"encoding/json"
+	"log"
+
+	"backend/tools/socket"
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
@@ -34,9 +39,10 @@ func GetContacts(c *fiber.Ctx) error {
 	})
 }
 
-//TODO - Handle errors
-//TODO - Logging
-
+// TODO - Handle errors
+// TODO - Logging
+// TODO - Retrieve messages
+// TODO - Find a better way to get email from connection-
 // Send message to the specified contact using websockets.
 func MessageHandler(c *websocket.Conn) {
 	var (
@@ -44,6 +50,19 @@ func MessageHandler(c *websocket.Conn) {
 		message []byte
 		err     error
 	)
+
+	user := c.Query("email")
+
+	if user == "" {
+		log.Print("Failed to connect to client")
+		c.WriteMessage(websocket.TextMessage, []byte("email param missing"))
+		return
+	}
+
+	socket.NewConnection(user, c)
+
+	err = c.WriteMessage(mt, []byte("Successful connection"))
+
 	for {
 		mt, message, err = c.ReadMessage()
 
@@ -51,9 +70,26 @@ func MessageHandler(c *websocket.Conn) {
 			break
 		}
 
-		err = c.WriteMessage(mt, message)
-		err = c.WriteMessage(mt, []byte("Te has unido al chat"))
+		var m models.Message
+		err = json.Unmarshal(message, &m)
+		if err != nil {
+			break
+		}
 
+		var addressee string
+		if m.GetTransmitter() == "user" {
+			addressee = m.GetIdTrainer()
+		} else {
+			addressee = m.GetIdUser()
+		}
+
+		addrConn := socket.GetConnection(addressee)
+		if addrConn != nil {
+			addrConn.WriteMessage(websocket.TextMessage, message)
+		}
+
+		controllers.CreateMessage(db.Orm(), &m)
 	}
 
+	socket.RemoveConnection(user)
 }
