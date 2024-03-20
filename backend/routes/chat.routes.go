8ade48711ddca5196d032b74e7c22ca973a9fc6f
Arturo González Bretón <arturo.gbreton@ciencias.unam.mx>
2024-03-20 00:14:00 -0600

    Fix GetContacts and update error handling in ChatHandler function

--- backend/routes/chat.routes.go
+++ backend/routes/chat.routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"backend/controllers"
 	"backend/models"
+	"backend/tools"
 	"backend/tools/db"
 	"encoding/json"
 	"log"
@@ -11,15 +12,49 @@ import (
 	"backend/tools/socket"
 
 	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
 )
 
 // TODO - Implement function
 
-// Returns the user contacts based on the user session.
-// Returns 500 HTTP Status Code if the session can't get
-// retrieved or an error occurs when querying the contact list.
-func getContacts(id uint64) (string, error) {
-	return "", nil
+// Returns the user contact list based on the user session.
+// Returns 418 if the user is not logged in.
+// Returns 500 HTTP Status Code if tan error occurs when
+// querying the contact list.
+func GetContacts(c *fiber.Ctx) error {
+	dbase := db.Orm()
+	logged, id := tools.GetCurrentUserId(c)
+
+	if !logged {
+		return controllers.ApiError(c, "I'm a teapot", 418)
+	}
+
+	type user_contact struct {
+		Id    uint64
+		Alias string
+	}
+
+	var contacts []user_contact
+
+	result := dbase.Raw("SELECT "+
+		"contacts.id_contact AS id, "+
+		"base_users.alias AS alias "+
+		"FROM "+
+		"contacts "+
+		"JOIN "+
+		"base_users "+
+		"ON "+
+		"contacts.id_contact = base_users.id "+
+		"WHERE "+
+		"contacts.id_user = ?", id).Scan(&contacts)
+
+	if result.Error != nil {
+		return controllers.ApiError(c, "Failed to retrieve user contacts", 500)
+	}
+
+	return c.JSON(fiber.Map{
+		"contacts": contacts,
+	})
 }
 
 // TODO - Handle errors
@@ -64,7 +99,14 @@ func ChatHandler(c *websocket.Conn) {
 		var m models.Message
 		err = json.Unmarshal(message, &m)
 		if err != nil {
-			break
+			c.WriteMessage(websocket.TextMessage, []byte("{\"error\":\"The message could not be parsed\"}"))
+			continue
+		}
+
+		err = controllers.CreateMessage(db.Orm(), &m)
+		if err != nil {
+			c.WriteMessage(websocket.TextMessage, []byte("{\"error\":\"The message could not be saved\"}"))
+			continue
 		}
 
 		addressee := m.GetIdAddressee()
@@ -73,8 +115,6 @@ func ChatHandler(c *websocket.Conn) {
 		if addrConn != nil {
 			addrConn.WriteMessage(websocket.TextMessage, message)
 		}
-
-		controllers.CreateMessage(db.Orm(), &m)
 	}
 
 	socket.RemoveConnection(userId)
