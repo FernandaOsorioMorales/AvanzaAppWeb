fbcf22d9a80486b84a8a41276e8da29a26ca4690
Fernanda <fernanda_osorio@ciencias.unam.mx>
2024-03-19 22:44:26 -0600

    login listo

--- frontend/src/routes/register.tsx
+++ frontend/src/routes/register.tsx
@@ -3,24 +3,34 @@ import React from "react";
 const RegisterForm : React.FC =() =>{
     return(
         <div>
-            <form className="p-56">
+            <form className="p-56" onSubmit={submitData}>
                 <h1 className="text-5xl text-center">Regístrate</h1>
+
                 <h2 className="text-2xl text-center text-white">Tu nombre</h2>
                 <div className="input-box flex justify-center">
-                    <input type= "text" placeholder="Nombres" required/>
+                    <input type= "text" onInput={e=>setName(e.target.value)} placeholder="Nombres"  />
                 </div>
-                <h2 className="text-2xl text-center text-white">Tu apellido paterno</h2>
-                <div className="input-box flex justify-center">
-                    <input type= "text" placeholder="Apellido paterno" required/>
-                </div>
-                <h2 className="text-2xl text-center text-white">Tu apellido materno</h2>
-                <div className="input-box flex justify-center">
-                    <input type= "text" placeholder="Apellido materno" required/>
-                </div>
+
                 <h2 className="text-2xl text-center text-white">Tu correo</h2>
                 <div className="input-box flex justify-center">
-                    <input type= "mail" placeholder="Correo@example.com" required/>
+                    <input type= "mail" onInput={e=>setEmail(e.target.value)} placeholder="Correo@example.com"  />
                 </div>
+
+                <h2 className="text-2xl text-center text-white">Tu número</h2>
+                <div className="input-box flex justify-center">
+                    <input type= "text" onInput={e=>setPhone(e.target.value)} placeholder="55 5555 5555"  />
+                </div>
+
+                <h2 className="text-2xl text-center text-white">Tu fecha de nacimiento</h2>
+                <div className="input-box flex justify-center">
+                    <input type="date" onInput={e=>setBirthDate(e.target.value)}   />
+                </div>
+
+                <h2 className="text-2xl text-center text-white">Tu contraseña</h2>
+                <div className="input-box flex justify-center">
+                    <input type="password" onInput={e=>setPassword(e.target.value)}    />
+                </div>
+
                 <div className="flex justify-center">
                 <button type="submit" className="text-black text-1xl text-center bg-white p-2 rounded-sm">Registrar</button>
                 </div>
