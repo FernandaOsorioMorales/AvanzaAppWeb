536ba2c86874df0d17664fd0af66b59a7d26ae38
Emiliano López <emilianolp@ciencias.unam.mx>
2024-04-14 01:46:08 -0600

    RelativePosition

--- frontend/src/components/routineAssembler/ListRoutines.tsx
+++ frontend/src/components/routineAssembler/ListRoutines.tsx
@@ -20,50 +20,49 @@ export function Routine(params: {routineName: string, tags: string[]}){
             <Modal open={ModalOpen} >
                         <CDRoutine onClose={() => setModalOpen(false)} RoutineName={params.routineName} Tags={params.tags} Exercises={""}></CDRoutine>
             </Modal>
-            
-            <div className="w-full h-20 mb-6 rounded flex flex-row justify-around items-center bg-blue-100">
-                <h1 className="w-1/4 flex justify-center items-center h-16 text-xl text-gray-600">{params.routineName}</h1>
-                <ul className="w-1/3 flex flex-row justify-around">
-                    {params.tags.map((tag, index) => <li key={index} className=" p-2 text-xs font-medium me-2 px-2.5 py-0.5 rounded bg-gray-600 text-gray-300">{tag}</li>)}
-                </ul>
 
-                <div className="w-fit flex justify-between">
-                    <button className="h-12 mr-6 py-2.5 rounded px-5 me-2 mb-2 text-sm font-medium focus:z-10 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700">
-                        Compartir
-                    </button>
+            <div className="w-full h-20 mb-6 rounded flex flex-row justify-around items-center bg-blue-100 relative">
+                <div className="flex flex-row absolute left-8 w-10/12">
+                    <h1 className="w-1/3 flex justify-center items-center h-16 text-xl text-gray-600">
+                        {params.routineName}
+                    </h1>
+                    <ul className="w-full flex flex-row justify-center items-center">
+                        {params.tags.map((tag, index) => <li key={index} className=" p-2 h-6 text-xs font-medium me-2 px-2.5 py-0.5 rounded bg-gray-600 text-gray-300">{tag}</li>)}
+                    </ul>
+                </div>
 
-                    <button onClick={() => setModalOpen(true)} className="h-12 mr-6 py-2.5 rounded px-5 me-2 mb-2 text-sm font-medium focus:z-10 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700">
+                <div className="w-fit flex justify-between items-center absolute right-2">
+                    <button onClick={() => setModalOpen(true)} className="h-12 py-2.5 mr-3 rounded px-5 text-sm font-medium focus:z-10 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700">
                         Editar
                     </button>
-                </div>
-                
-                <div className="relative">
-                    <button onClick={() => setIsOpen(!isOpen)}>
-                        <EllipsisVertical size={25}/>
-                    </button>
+                    <div className="relative">
+                        <button onClick={() => setIsOpen(!isOpen)}>
+                            <EllipsisVertical size={25}/>
+                        </button>
 
-                    {isOpen && (
-                        <div className="absolute z-10 mt-2 w-24 bg-white rounded-md shadow-lg" onMouseLeave={() => setIsOpen(false)}>
-                            <ul className="list-reset">
-                                <li>
-                                    <button
-                                        onClick={() => alert("Duplicar :)")}
-                                        className="block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white"
-                                        >
-                                        <p>Duplicar</p>
-                                    </button>
-                                </li>
-                                <li>
-                                    <button
-                                        onClick={() => alert("Ha elegido el camino de la muerte")}
-                                        className="block px-4 py-2 text-gray-800 hover:bg-red-500 hover:text-white"
-                                        >
-                                        <p>Eliminar</p>
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-                    )}
+                        {isOpen && (
+                            <div className="absolute z-10 mt-2 w-24 bg-white rounded-md shadow-lg" onMouseLeave={() => setIsOpen(false)}>
+                                <ul className="list-reset">
+                                    <li>
+                                        <button
+                                            onClick={() => alert("Duplicar :)")}
+                                            className="block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white"
+                                            >
+                                            <p>Duplicar</p>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            onClick={() => alert("Ha elegido el camino de la muerte")}
+                                            className="block px-4 py-2 text-gray-800 hover:bg-red-500 hover:text-white"
+                                            >
+                                            <p>Eliminar</p>
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
