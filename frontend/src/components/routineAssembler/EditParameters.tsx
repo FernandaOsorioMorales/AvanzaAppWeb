49b2729cbfc4a5548e711ee35cf92508c600138b
Emiliano López <emilianolp@ciencias.unam.mx>
2024-04-28 14:23:29 -0600

    FethingRoutines #27

--- frontend/src/components/routineAssembler/EditParameters.tsx
+++ frontend/src/components/routineAssembler/EditParameters.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import { excercise } from './Exercise'
+import { exercise } from './Exercise'
 
-function transformExcercise(excercise: excercise, sets: number, reps: number): excercise {
+function transformExcercise(exercise: exercise, sets: number, reps: number): exercise {
   return {
-    ID: excercise.ID,
-    IdExcercise: excercise.IdExcercise,
-    Ordinal: excercise.Ordinal,
-    Name: excercise.Name,
+    ID: exercise.ID,
+    IdExercise: exercise.IdExercise,
+    Ordinal: exercise.Ordinal,
+    Name: exercise.Name,
     Reps: reps,
     Sets: sets
   }
 }
 
-export function EditParameters(params: {onClose: () => void, index: number, editExcercise: (index: number, excercise: excercise) => void, excercise: excercise}) {
-  const excercise = params.excercise
-  const [sets, setSets] = React.useState(params.excercise.Sets)
-  const [reps, setReps] = React.useState(params.excercise.Reps)
+export function EditParameters(params: {onClose: () => void, index: number, editExcercise: (index: number, exercise: exercise) => void, exercise: exercise}) {
+  const exercise = params.exercise
+  const [sets, setSets] = React.useState(params.exercise.Sets)
+  const [reps, setReps] = React.useState(params.exercise.Reps)
 
   return (
     <div className='h-full w-full'>
@@ -39,7 +39,7 @@ export function EditParameters(params: {onClose: () => void, index: number, edit
 
       <div className='flex justify-between'>
         <button onClick={params.onClose} className='bg-gray-500 text-white rounded p-2 m-2 hover:bg-red-500'>Cancelar</button>
-        <button onClick={() => {params.editExcercise(params.index, transformExcercise(excercise, sets, reps)); params.onClose()}} className='bg-gray-500 text-white rounded p-2 m-2 hover:bg-blue-500'> Guardar</button>
+        <button onClick={() => {params.editExcercise(params.index, transformExcercise(exercise, sets, reps)); params.onClose()}} className='bg-gray-500 text-white rounded p-2 m-2 hover:bg-blue-500'> Guardar</button>
       </div>
     </div>
   )
